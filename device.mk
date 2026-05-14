57ed17bc7e5a04bc7f0e3e6d27288bbbdcf3dd50
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-14 14:36:48 +0000

    gauguin: Add AxBurstEngine v2 configurations
    - Full rewrite and split layout migration for Qualcomm SD750G platform
    - Mapped CPU policies, Adreno GPU (kgsl-3d0) and LLCC memory bandwidth nodes
    - Enforce system_server ownership permissions via init.qcom.rc
    - Set pmqos latency character driver rules in ueventd.gauguin.rc
    
    Change-Id: I1db0444fe29d9e2172480a905cbbe29deb9468e8
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -436,7 +436,9 @@ PRODUCT_PACKAGES += \
     UpdaterResGauguinaxion
 # AxBurstEngine (Performance Config)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/axion/ax_perf_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/ax_perf_config.xml
-
+    device/xiaomi/gauguin/configs/axion/ax_perf_resources.xml:$(TARGET_COPY_OUT_VENDOR)/etc/ax_perf_resources.xml \
+    device/xiaomi/gauguin/configs/axion/ax_perf_boosts.xml:$(TARGET_COPY_OUT_VENDOR)/etc/ax_perf_boosts.xml \
+    device/xiaomi/gauguin/configs/axion/ax_perf_thermal.xml:$(TARGET_COPY_OUT_VENDOR)/etc/ax_perf_thermal.xml \
+    device/xiaomi/gauguin/configs/axion/ax_perf_threads.xml:$(TARGET_COPY_OUT_VENDOR)/etc/ax_perf_threads.xml
 # Qualcomm Specific Performance flags
 TARGET_DISABLES_LIBPERF := true
