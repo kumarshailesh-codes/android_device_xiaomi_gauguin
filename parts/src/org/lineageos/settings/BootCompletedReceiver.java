3233daf0ff2080413d5d7574de112fd57086bd35
kumarshailesh-codes <kumarshailesh12qw4@gmail.com>
2026-07-10 12:04:02 +0000

    gauguin: Integrate Bypass Charging Parts Settings app & QS tile
    - Reconstructs and adds standard-compliant XiaomiParts Settings App inside parts/.
    - Implements and registers the BypassChargingTile QS service.

---
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -0,0 +1,14 @@
+package org.lineageos.settings;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import org.lineageos.settings.bypasscharging.BypassChargingUtils;
+public class BootCompletedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            int mode = BypassChargingUtils.getBypassMode(context);
+            BypassChargingUtils.writeNode(mode);
+        }
+    }
+}
---
+++ parts/src/org/lineageos/settings/bypasscharging/BypassChargingActivity.java
@@ -0,0 +1,12 @@
+package org.lineageos.settings.bypasscharging;
+import android.app.Activity;
+import android.os.Bundle;
+public class BypassChargingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(
+                android.R.id.content,
+                new BypassChargingFragment(), "bypass_charging").commit();
+    }
+}
